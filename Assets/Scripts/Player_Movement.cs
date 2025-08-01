b2b70c7556499bfe604525558376ab1de98d8406
Gawindki <pgaw4933@gmail.com>
2025-08-01 03:03:14 +0200

    Spieler kann gehen und springen;
    Spieler kann sich mit A und D nach links und rechts bewegen. Man kann auch mit Leertaste springen.

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BetterTest : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveInput = Input.GetAxis("Horizontal");
+
+        // Bewegung
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        // Sprite flippen
+        if (moveInput > 0.01f)
+            spriteRenderer.flipX = false;
+        else if (moveInput < -0.01f)
+            spriteRenderer.flipX = true;
+
+        // Springen
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.y, jumpForce);
+        }
+
+        // Bodencheck
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if ( isGrounded == true)
+        {
+            Debug.Log("Player is Grounded");
+        } 
     }
 }
